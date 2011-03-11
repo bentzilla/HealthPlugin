f80f929ceafaa121ce142d89d51c97e64f2ce8c6
Richard <bentzilla@fsscripts.com>
2011-03-11 13:54:15 -0500

    Added RPG Style Chat Added Permissions TODO: Fix nodes

--- src/com/fsscripts/bentzilla/HPlayerChatListener.java
+++ src/com/fsscripts/bentzilla/HPlayerChatListener.java
@@ -14,15 +14,126 @@ public class HPlayerChatListener extends PlayerListener {
 	public void onPlayerChat(PlayerChatEvent event) {
 		Player player = event.getPlayer();
 		for (Player P : plugin.getServer().getOnlinePlayers()) {
-
-			if (Health.enabled(P)) {
-				ChatColor c;
-				if (player.getHealth() <= 10){
-					c = ChatColor.RED;
-				} else {
-					c = ChatColor.GREEN;
+			if (Health.hpChatEnabled(P)) {
+				ChatColor c1 = ChatColor.GREEN;
+				ChatColor c2 = ChatColor.GREEN;
+				ChatColor c3 = ChatColor.GREEN;
+				System.out.println(Health.chatHealthUsers.values().toString());
+				if (Health.chatHealthUsers.containsValue("off")){
+					if (player.getHealth() <= 10){
+						c1 = ChatColor.RED;
+					} else {
+						c1 = ChatColor.GREEN;
+					}
+					P.sendMessage("<" + player.getDisplayName() + "> [" + c1 + player.getHealth() + ChatColor.WHITE + "/" + ChatColor.GREEN + "20" +  ChatColor.WHITE + "]: "  + event.getMessage().toString());
+				} else if(Health.chatHealthUsers.containsValue("on")){
+					String s1 ="", s2 = "", s3 = "";
+					Integer h = player.getHealth();
+					System.out.println(h);
+					switch (h){
+					//case 20: 
+					case 19: s1 = "|||||||||";
+							 c2 = ChatColor.YELLOW;
+							 s2 = "|";
+							 break;
+					case 18: s1 = "||||||||";
+			 		 		 c3 = ChatColor.RED;
+			 		 		 s3 = "|";
+			 		 		 break;
+					case 17: s1 = "||||||||";
+					 		 c2 = ChatColor.YELLOW;
+					 		 s2 = "|";
+					 		 c3 = ChatColor.RED;
+					 		 s3 = "|";
+					 		 break;
+					case 16: s1 = "||||||||";
+	 		 		 		 c3 = ChatColor.RED;
+	 		 		 		 s3 = "||";
+	 		 		 		 break;
+					case 15: s1 = "|||||||";
+			 		 		 c2 = ChatColor.YELLOW;
+			 		 		 s2 = "|";
+			 		 		 c3 = ChatColor.RED;
+			 		 		 s3 = "||";
+			 		 		 break;
+					case 14: s1 = "|||||||";
+	 		 		 		 c3 = ChatColor.RED;
+	 		 		 		 s3 = "|||";
+	 		 		 		 break;
+					case 13: s1 = "||||||";
+							 c2 = ChatColor.YELLOW;
+							 s2 = "|";
+							 c3 = ChatColor.RED;
+							 s3 = "|||";
+							  break;
+					case 12: s1 = "||||||";
+	 		 		 		 c3 = ChatColor.RED;
+	 		 		 		 s3 = "||||";
+	 		 		 		 break;
+					case 11: s1 = "|||||";
+			 		 	 	 c2 = ChatColor.YELLOW;
+			 		 	 	 s2 = "||||";
+			 		 	 	 c3 = ChatColor.RED;
+			 		 	 	 s3 = "|";
+			 		 	 	 break;
+					case 10: s1 = "|||||";
+	 		 		 		 c3 = ChatColor.RED;
+	 		 		 		 s3 = "|||||";
+	 		 		 		 break;
+					case 9: s1 = "||||";
+							c2 = ChatColor.YELLOW;
+							s2 = "|||||";
+							c3 = ChatColor.RED;
+							s3 = "|";
+							break;
+					case 8: s1 = "||||";
+	 		 		 		c3 = ChatColor.RED;
+	 		 		 		s3 = "||||||";
+	 		 		 		break;
+					case 7: s1 = "|||";
+							c2 = ChatColor.YELLOW;
+							s2 = "|";
+							c3 = ChatColor.RED;
+							s3 = "||||||";
+							break;
+					case 6: s1 = "|||";
+	 		 		 		c3 = ChatColor.RED;
+	 		 		 		s3 = "|||||||";
+	 		 		 		break;
+					case 5: s1 = "||";
+							c2 = ChatColor.YELLOW;
+							s2 = "|";
+							c3 = ChatColor.RED;
+							s3 = "|||||||";
+							break;
+					case 4: s1 = "||";
+	 		 		 		c3 = ChatColor.RED;
+	 		 		 		s3 = "||||||||";
+	 		 		 		break;
+					case 3: s1 = "|";
+			 		 		c2 = ChatColor.YELLOW;
+			 		 		s2 = "|";
+			 		 		c3 = ChatColor.RED;
+			 		 		s3 = "||||||||";
+			 		 		break;
+					case 2: s1 = "|";
+	 		 		 		c3 = ChatColor.RED;
+	 		 		 		s3 = "|||||||||";
+	 		 		 		break;
+					case 1: s1 = "";
+							c2 = ChatColor.YELLOW;
+							s2 = "|";
+							c3 = ChatColor.RED;
+							s3 = "|||||||||";
+							break;
+					case 0: s1 = "";
+	 		 		 		c3 = ChatColor.RED;
+	 		 		 		s3 = "||||||||||";
+	 		 		 		break;
+					}
+					System.out.println(c1 + s1 + c2 + s2 + c3 + s3);
+					P.sendMessage("[" + c1 + s1 + c2 + s2 + c3 + s3 + ChatColor.WHITE + "]: <" + player.getDisplayName() + "> "  + event.getMessage().toString());
 				}
-				P.sendMessage("<" + player.getDisplayName() + "> [" + c + player.getHealth() + ChatColor.WHITE + "/" + ChatColor.GREEN + "20" +  ChatColor.WHITE + "]: "  + event.getMessage().toString());
 			} else {
 				P.sendMessage("<" + player.getDisplayName() + "> " + event.getMessage().toString());
 			}	
--- src/com/fsscripts/bentzilla/Health.java
+++ src/com/fsscripts/bentzilla/Health.java
@@ -3,63 +3,135 @@ package com.fsscripts.bentzilla;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import org.bukkit.Location;
 import org.bukkit.Server;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginLoader;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.PluginManager;
+import com.nijikokun.bukkit.Permissions.Permissions;
 
 public class Health extends JavaPlugin {
-    public final static HashMap<Player, ArrayList<Block>> healthUsers = new HashMap();  
+    
+	private String HVersion = "1.0";
+	
+    public final static HashMap<Player, String> chatHealthUsers = new HashMap<Player,String>(); 
     private final HPlayerChatListener ThePlayerListener = new HPlayerChatListener(this);
+    //private final HEntityListener TheEntityListener = new HEntityListener(this);
+    private Permissions HPermissions = null;
     
 	public void onDisable() {
-		System.out.println("HealthPlugin Disabled");
+		System.out.println("HealthPlugin v" + HVersion + " Disabled");
 	}
 
 	public void onEnable() {
+		setupPermissions();
 		PluginManager pm = getServer().getPluginManager();
 		pm.registerEvent(Event.Type.PLAYER_CHAT, this.ThePlayerListener, Event.Priority.Normal, this);
+		//pm.registerEvent(Event.Type.ENTITY_DAMAGED, this.TheEntityListener, Event.Priority.Highest, this);
 		PluginDescriptionFile pdfFile = this.getDescription();
 		System.out.println(pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!");
 	}
+	
+    private void setupPermissions() {
+        Plugin permissionsPlugin = this.getServer().getPluginManager().getPlugin("Permissions");
+        PluginDescriptionFile pdfFile = this.getDescription();
+
+        if (permissionsPlugin != null) {
+           System.out.println(pdfFile.getName() + ": Using Nijikokun's permissions plugin for permissions");
+            this.HPermissions = ((Permissions) permissionsPlugin);
+        }
+    }
+    
+    public Permissions getPermissions() {
+        return this.HPermissions;
+    }
 
 	 public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-			if (!(sender instanceof Player)) {
+			
+		 if (HPermissions == null) {
+             sender.sendMessage("Cannot find Permissions plugin.");
+             return false;
+         }
+		 
+		 if (!(sender instanceof Player)) {
 				return false;
-			}
+		 }
 			
-			Player player = (Player)sender;
-			String command = "/" + commandLabel + " " + join(args, 0);
-			
-			boolean action = false;
-			if (!action) {
-					action = toggleHealth(player, command);
-			}
-			return action;
-	    }
+		String command = "/" + commandLabel + " " + join(args, 0);
+		
+		System.out.println("Command = " + command);
+    	System.out.println("chathealth, there are current: " + args.length + " args");
+    	System.out.println(command.toLowerCase());
+		
+		if (!command.toLowerCase().contains("/hp ")) {
+            return false;
+        }
+
+        if (args.length < 1) {
+            return false;
+        }
+
+        Player user = (Player) sender;
+        String subCommand = args[0].toLowerCase();
+        System.out.println("Sibcom: " + subCommand);
+
+        boolean hasPermission = this.HPermissions == null ? false : Permissions.Security.permission(user, "hp.admin");
+
+        if (!user.isOp() && !hasPermission) {
+            sender.sendMessage("You do not have permission to use this command.");
+            return true;
+        }
+
+        if (subCommand.equals("chathealth")) {
+               	
+        	System.out.println("chathealth, there are current: " + args.length + " args");
+        	String rpgstyle = "off";
+            if (args.length == 2){
+            	if (args[1].toLowerCase().equals("on")){
+                	rpgstyle = args[1].toLowerCase();
+            	}else if(args[1].toLowerCase().equals("off")){
+                	rpgstyle = args[1].toLowerCase();
+            	}else{
+            		user.sendMessage("Example Usage: /hp chathealth on");
+            	}
+            }else if (args.length == 1){
+            	toggleChatHealth(user, rpgstyle);
+            	return true;
+            }
+        	if (!hpChatEnabled(user)){
+        		toggleChatHealth(user, rpgstyle);
+        	}
+        	if (!chatHealthUsers.values().toString().contains(rpgstyle)){
+        		Health.chatHealthUsers.remove(user);
+        		Health.chatHealthUsers.put(user, rpgstyle);
+        	}
+
+        	return true;
+        }
+        return false;
+	 }
 	   
-	    public static boolean enabled(Player player) {
-			return healthUsers.containsKey(player);
+	    public static boolean hpChatEnabled(Player player) {
+			return chatHealthUsers.containsKey(player);
 		}
 
-	    public boolean toggleHealth(Player player, String command){
-	    	if (command.toLowerCase().contains("/showhealth") || command.toLowerCase().contains("/health")) {
-		    	if(enabled(player)) {
-		    		this.healthUsers.remove(player);
-		    		player.sendMessage("HealthPlugin disabled");
-		    	} else {
-		    		this.healthUsers.put(player, null);
-		    		player.sendMessage("HealthPlugin enabled");
-		    	}		
-		    	return true;
-		    }
-	    	return false;
+	    public boolean toggleChatHealth(Player player, String style){
+	    	if(hpChatEnabled(player)) {
+	    		Health.chatHealthUsers.remove(player);
+	    		player.sendMessage("Health in Chat DISABLED");
+	    	} else {
+	    		Health.chatHealthUsers.put(player, style);
+	    		player.sendMessage("Health in Chat ENABLED");
+	    	}		
+	    	return true;
 	    }
     
     public static String join(String[] arr, int offset) {
